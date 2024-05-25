4c39c5a7928bd7ba5fb36581780b17bc467d9f06
Sebastian Fernandez <seb.flipflip@gmail.com>
2024-05-25 16:48:52 -0600

    pass

--- utils/global_values.js
+++ utils/global_values.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 
-const password = 'S1f2L3123sfl';
+const password = 'root';
 const destination_folder_copy = path.join(require('os').homedir(), 'Downloads', 'copied_files');
 const destination_folder_recover = path.join(require('os').homedir(), 'Downloads', 'recovered_files');
 
